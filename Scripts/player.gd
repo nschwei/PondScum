90ba72e87ca920a14726eb81cdcbcd153532d20a
nschwei <46508723+nschwei@users.noreply.github.com>
2024-05-13 10:54:58 -0500

    changes to movement
    Keep velocity in air. Flip if move in launch dir and hold left click for side flip

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,8 +13,10 @@ signal score_points(points)
 
 var movement_velocity: Vector3
 var rotation_direction: float
+var char_facing: Vector2
 var gravity = 0
 var on_floor = false
+var calc_dir = false
 
 ### TRICK TIME ###
 var rot_track_x = 0
@@ -80,27 +82,34 @@ func handle_controls(delta):
 	
 	input = input.normalized()
 	
-	#if is_on_floor():
-	movement_velocity = input * movement_speed * delta
+	if is_on_floor():
+		movement_velocity = input * movement_speed * delta
 	
 	# jumping
 	if Input.is_action_just_pressed("jump"):
 		if (is_on_floor()): jump()
 		
 	# Reverse Hold button to reverse direction
-	if Input.is_action_just_pressed("reverse"):
-		turn_speed = -turn_speed
+	#if Input.is_action_just_pressed("reverse"):
+	#	turn_speed = -turn_speed
 	# Return to normal on key release
-	if Input.is_action_just_released("reverse"):
-		turn_speed = - turn_speed
+	#if Input.is_action_just_released("reverse"):
+	#	turn_speed = - turn_speed
 		
 	# tricking
 	if not is_on_floor():
-		
+		var input_dir = Vector2(input.x, input.z).normalized()
+		#char_facing = Vector2(model.transform.basis.z.x, model.transform.basis.z.z).normalized()
+		var rot_mod = input_dir.dot(char_facing)
+		print(char_facing)
+		#if rot_mod >= 0:
+		#	rot_mod = 1
+		#else: 
+		#	rot_mod = -1
 		# input for spin directions
-		if Input.is_action_pressed("front_flip"):
-			model.rotate_object_local(Vector3(1,0,0), turn_speed*delta)
-			rot_track_x += (turn_speed*delta)
+		if Input.is_action_pressed("move_forward") or Input.is_action_pressed("move_back"):
+			model.rotate_object_local(Vector3(1,0,0), turn_speed*delta*rot_mod)
+			rot_track_x += (turn_speed*delta*rot_mod)
 		if Input.is_action_pressed("side_flip"):
 			model.rotate_object_local(Vector3(0,0,1), turn_speed*delta)
 			rot_track_y += (turn_speed*delta)
@@ -136,12 +145,12 @@ func handle_gravity(delta):
 	if gravity > 0 and is_on_floor():
 		# stop apply force and let us spin again
 		gravity = 0
+		char_facing = Vector2(model.transform.basis.z.x, model.transform.basis.z.z).normalized()
 		if not on_floor:
 			landing()
 			
 func landing():
 	on_floor = true
-	
 	# reset tracking
 	rot_track_x = 0
 	rot_track_y = 0
