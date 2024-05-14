dd4775080fdb9e774709f7f24684ad2c20002bd5
nschwei <46508723+nschwei@users.noreply.github.com>
2024-05-13 22:01:25 -0500

    Added score win limit and hide trick score on ground

--- Scenes/CanvasLayer.gd
+++ Scenes/CanvasLayer.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
 
+@onready var total_score = $TotalScorePanel/MarginContainer/VBoxContainer/TotalScore
+@onready var current_score = $CurrentScorePanel/MarginContainer/CurrentScore
+@onready var current_score_panel = $CurrentScorePanel
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,16 +17,23 @@ func _process(delta):
 
 func _on_player_score_points(points, bonus_string):
 	# Add to the total score with the new trick score
-	var new_score = int($TotalScorePanel/MarginContainer/VBoxContainer/TotalScore.text)
+	var new_score = int(total_score.text)
 	new_score += points
-	$TotalScorePanel/MarginContainer/VBoxContainer/TotalScore.text = str(new_score)
+	total_score.text = str(new_score)
+	
 	# Add text for the landing upright multiplayer if it landed
-	$CurrentScorePanel/MarginContainer/CurrentScore.text += bonus_string
+	current_score.text += bonus_string
+	
 	# Wait and then clear trick
 	await get_tree().create_timer(1.0).timeout
-	$CurrentScorePanel/MarginContainer/CurrentScore.text = ""
+	current_score.text = ""
+	current_score_panel.visible = false
+	
+	# Update score for tracking win
+	get_owner().score = new_score
 
 
 func _on_player_trick_score(points):
 	# Show the current score for the ongoing trick
-	$CurrentScorePanel/MarginContainer/CurrentScore.text = str(points)
+	current_score_panel.visible = true
+	current_score.text = str(points)
--- Scripts/Game_Manager.gd
+++ Scripts/Game_Manager.gd
@@ -1,13 +1,18 @@
 extends Node
 
 var score = 0
+
+@export var win_score = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	#score = int($PixelView/SubViewport/CanvasLayer/TotalScorePanel/MarginContainer/VBoxContainer/TotalScore.text)
+	if score >= win_score:
+		print("BIIG WINNER")
 
 func score_points(points):
 	print('scored points')
