2de28e974f2900a5d8d779850037dceb00be12a7
nschwei <46508723+nschwei@users.noreply.github.com>
2024-05-14 14:25:02 -0500

    simple pause menu

--- Scenes/CanvasLayer.gd
+++ Scenes/CanvasLayer.gd
@@ -1,9 +1,9 @@
 extends CanvasLayer
 
-
 @onready var total_score = $TotalScorePanel/MarginContainer/VBoxContainer/TotalScore
 @onready var current_score = $CurrentScorePanel/MarginContainer/CurrentScore
 @onready var current_score_panel = $CurrentScorePanel
+@onready var pause_menu = $PausePanel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,8 +12,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	check_toggle_pause()
 
+func pause():
+	pause_menu.visible = !pause_menu.visible
+	get_tree().paused = !get_tree().paused
+
+func check_toggle_pause():
+	if Input.is_action_just_pressed("pause"):
+		pause()
 
 func _on_player_score_points(points, bonus_string):
 	# Add to the total score with the new trick score
@@ -37,3 +44,10 @@ func _on_player_trick_score(points):
 	# Show the current score for the ongoing trick
 	current_score_panel.visible = true
 	current_score.text = str(points)
+
+func _on_resume_pressed():
+	pause()
+	print("AAA")
+
+func _on_quit_pressed():
+	get_tree().quit()
